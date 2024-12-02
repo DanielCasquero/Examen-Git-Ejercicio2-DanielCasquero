ad0dc208266fa762cc1bcd25e17b9a20dfaeca29
Daniel Casquero <24.daw.danielcasquero@colegiomontessori.com>
2024-12-02 19:56:43 +0100

    AreaCuadrado

--- Prouecto1/src/area/Area.java
+++ Prouecto1/src/area/Area.java
@@ -6,9 +6,9 @@ public class Area {
 
 	public static void main(String[] args) {
 		Scanner chistoseria = new Scanner(System.in);
-		System.out.println("Introduzca el radio de un circulo");
-		double radio = chistoseria.nextDouble();
-		System.out.println("El area del circulo es: " +  Math.PI *radio * radio );
+		System.out.println("Introduzca el lado de un cuadrado");
+		double lado = chistoseria.nextDouble();
+		System.out.println("El area del cuadrado es: " +  lado * lado );
 		
 		chistoseria.close();
 	}
